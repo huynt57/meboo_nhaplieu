bad2b99d76644fd530f5434253bc81d547aca5b3
huynt <huynt57@gmail.com>
2015-09-23 11:47:09 +0700

    check user login

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -2,6 +2,10 @@
 
 class UserController extends Controller {
 
+    public function beforeController($action)
+    {
+        $this->checkLogin();
+    }
     public function actionIndex() {
         $this->render('index');
     }
